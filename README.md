b614301b8c4f3297ed5e6bc5378634e61966e912
Ben Collier <bcollier@biolinux-Latitude-E6320>
2015-01-05 15:06:54 +0000

    Final changes for submission to PyPI.

--- README.md
+++ README.md
@@ -3,10 +3,16 @@ vcloudpy
 
 Functions for interfacing with VMware vCloud (both private cloud and vCloud Air).
 
-vCloud Suite, vCloud Air and vCloud Director are registered trademarks of VMware, Inc.
+This system is rather rudimentary at the moment, but does provide workable functionality for making remote changes to VApps from within Python. The system also includes a server module, which will launch a Tornado instance and respond to API requests from a web application.
+
+More documentation to follow in later releases.
 
 Contributors
 ------------
 
 Ben Collier
 
+Legal Notes
+-----------
+
+vCloud Suite, vCloud Air and vCloud Director are registered trademarks of VMware, Inc.
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='vctools',
-    version='0.1',
+    version='0.1.5',
     author='B. Collier',
     author_email='bmcollier@gmail.com',
     packages=['vctools', 'vctools.test'],
-    scripts=['bin/stowe-towels.py'],
+    keywords=['vCloud', 'VMware', 'API', 'Director', 'Air'],
     url='http://pypi.python.org/pypi/TowelStuff/',
     license='LICENSE.md',
-    description='A set of tools for managing vmWare vCloud, whether on vCloud Air or locally installed.',
+    description='A set of tools for interacting with the VMware vCloud API, whether on vCloud Air or locally installed.',
     long_description=open('README.md').read(),
     install_requires=[
         "requests >= 2.5.1",
